04dda52cae6bb02f7a2efa4266a3d67f3d28268e
YS <87403433+AndyChoi4495@users.noreply.github.com>
2026-07-31 14:48:00 -0400

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        ''' 
+        
+        binary search 
+        find the row first 
+        after find using mid pointer
+
+        '''
+        
+        top = 0
+        bot = len(matrix) -1
+
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        l = 0
+        r = len(matrix[0]) - 1
+    
+        row = (top + bot) // 2
+        while l <= r:
+            m = (l + r) // 2
+            if target == matrix[row][m]:
+                return True
+            elif target > matrix[row][m]:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return False
