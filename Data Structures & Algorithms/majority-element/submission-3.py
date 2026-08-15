f33ced8ab30738d6b1699d6f5c192104890b10e9
YS <87403433+AndyChoi4495@users.noreply.github.com>
2026-08-14 23:00:34 -0400

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ''' 
+        only works at majority number is exist 
+        Boyer-Moore Voting Algorithm will be worked
+        '''
+        
+        res, count = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else - 1)
+        
+        return res
+
